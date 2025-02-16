d093950c0863fd4582bd5066a30941a9569c3116
Noah's Tech <89303045+thatonelilguyfromtechtok@users.noreply.github.com>
2025-02-16 15:10:27 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,8 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	handleHttpError: PrerenderHttpErrorHandlerValue,
+	handleHttpError: PrerenderHttpErrorHandlerValue
+
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
